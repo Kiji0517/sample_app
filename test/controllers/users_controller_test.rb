d6844996aea3bb9fe9582a2ec6e83ece2859d364
Kiji0517 <ky9_mr30@yahoo.co.jp>
2023-08-22 02:18:03 +0000

    rails10.3

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -14,7 +14,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect index when not logged in" do
     get users_path
-    assert_redirected_to login_url #ここエラー起きてる
+    assert_redirected_to login_url
   end
 
   test "should redirect edit when logged in as wrong user" do
@@ -23,6 +23,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
 
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
