171e0102384a1f72877c55f52d4e278c207b30e2
Kiji0517 <ky9_mr30@yahoo.co.jp>
2023-08-23 05:06:40 +0000

    rails13.2.1fin

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # 指定のワーカー数でテストを並列実行する
